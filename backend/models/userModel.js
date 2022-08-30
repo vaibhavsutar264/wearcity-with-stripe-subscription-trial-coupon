91f40a6641417e887ff83c9d15cadeee25a62ead
vaibhavsutar264 <vaibhavsutar264@gmail.com>
2022-08-30 19:22:26 +0530

    admin to user created from user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        default: "admin"
+        default: "user"
     },
     createdAt: {
         type: Date,
